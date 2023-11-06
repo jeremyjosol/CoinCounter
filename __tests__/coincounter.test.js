4a0299fa6a4acdcd695b30d8b6a0cbe07694dea6
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-06 11:16:18 -0800

    Pass test for userInput = 0 case

--- __tests__/coincounter.test.js
+++ __tests__/coincounter.test.js
@@ -5,4 +5,14 @@ describe('countChangeRecursively', () => {
     const userInput = countChangeRecursively("!");
     expect(userInput).toEqual("Please enter a valid number.");
   });
+
+  test('it should return all coin property values as 0 if 0 is passed in', () => {
+    const userInput = countChangeRecursively(0);
+    expect(userInput).toEqual({
+      quarters: 0,
+      dimes: 0,
+      nickels: 0,
+      pennies: 0
+    });
+  });
 });
--- src/js/coincounter.js
+++ src/js/coincounter.js
@@ -4,12 +4,12 @@ export function countChangeRecursively(change) {
   if (isNaN(change)) {
     return "Please enter a valid number.";
   
-  // } else if (change === 0) {
-  //   return {
-  //     quarters: 0,
-  //     dimes: 0,
-  //     nickels: 0,
-  //     pennies: 0
-  //   };
+  } else if (change === 0) {
+    return {
+      quarters: 0,
+      dimes: 0,
+      nickels: 0,
+      pennies: 0
+    };
   }
 } 
